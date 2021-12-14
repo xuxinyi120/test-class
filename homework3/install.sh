af67b9c5a81389fd94ee57af2badc29c9a2240da
xuxinyi120 <308434557@qq.com>
2021-12-14 11:28:57 +0800

    homework

---
+++ homework3/install.sh
@@ -0,0 +1,1 @@
+sudo apt-get install texlive-latex-recommended texlive-latex-extra texlive-fonts-extra  texlive-fonts-recommended
---
+++ homework3/run.sh
@@ -0,0 +1,2 @@
+# latex main.tex; bibtex main ; latex main.tex;latex main.tex; dvipdfm main.dvi
+pdflatex main.tex; bibtex main ; pdflatex main.tex; pdflatex main.tex
